46c5941e6e3b7fb2bd369afe21a3ed74c71ef257
eng-pcdias <111535533+eng-pcdias@users.noreply.github.com>
2022-12-08 11:07:26 -0300

    funcaoPrintf.cpp
    Como imprimir caracteres especiais através da função printf.

---
+++ funcaoPrintf.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	int reajuste = 10;    //cria a constante 'reajuste' e atribui valor '10' a ela.
+	printf("O reajuste foi de %d%%\n", reajuste);   //imprime o caractere especial '%' ao numero '10'.
+
+	system("pause");    //necessario para que a funcao 'printf' fique exibida na tela, necessario o cabecalho <stdlib.h>
+	return 0;
+}
